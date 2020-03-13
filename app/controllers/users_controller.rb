b476c70543afe7045fbd4420164c849c8d314d8a
daichi0410 <59831107+daichi0410@users.noreply.github.com>
2020-03-13 13:49:44 +0900

    default

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+  def show
+    user = User.find(params[:id])
+    @nickname = user.nickname
+    @tweets = user.tweets.page(params[:page]).per(5)
+  end
+
+end
